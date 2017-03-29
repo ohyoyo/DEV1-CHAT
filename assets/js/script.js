22fb0670a28ff86379e6e926e45a8d37088f2710
Yoann Di Maria <dm.yoann@gmail.com>
2017-03-30 01:31:17 +0200

    creation of the login page

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#buttonConnect').click(function() {
+        $('section#welcome').hide('clip', 500);
+        $('.loadPage').show();
+    });
+});
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var express = require('express');
-var app =  express();
-var server =  require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = 1337;
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io')(server);
+var port    = 1337;
 
-app.use('/css', express.static(__dirname + '/assets/css'));
-app.use('/js', express.static(__dirname + '/assets/js'));
-app.use('/images', express.static(__dirname + '/assets/images'));
-app.use('/fonts', express.static(__dirname + '/assets/fonts'));
+app.use('/src/css/', express.static(__dirname + '/assets/css'));
+app.use('/src/js/', express.static(__dirname + '/assets/js'));
+app.use('/src/img/', express.static(__dirname + '/assets/img'));
+app.use('/src/fonts/', express.static(__dirname + '/assets/fonts'));
 app.use('/', express.static(__dirname + '/views'));
 
-io.on('connection', function (socket){
+io.on('connection', function(socket) {
     //new connection
     console.log('nouvelle connection sur socket');
     console.log(socket.id);
 });
 
 server.listen(port);
-console.log("application work on port " + port);
+console.log('application work on port ' + port);
